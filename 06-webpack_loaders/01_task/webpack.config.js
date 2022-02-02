818f472db802f5d28694e0978dc0d4fca8ce16d4
Надежда <grennad991@gmail.com>
2022-02-02 20:10:37 +0200

    changed limit settings

--- 06-webpack_loaders/01_task/webpack.config.js
+++ 06-webpack_loaders/01_task/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8000,
+              limit: 8192,
               name: '[name].[ext]',
               outputPath: 'images',
             },
