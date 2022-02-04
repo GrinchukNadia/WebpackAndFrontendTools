d615bf3984593e17a449fccea2b270fc6a522112
Надежда <grennad991@gmail.com>
2022-02-04 12:46:36 +0200

    worked with webpack dev server

---
+++ 08-webpack-devserver/01_task/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e,o,t,n,c;o=(e={name:"Tom",location:"The World"}).name,t=e.location,n=document.querySelector(".profile__name"),c=document.querySelector(".profile__location"),n.textContent=o,c.textContent="from ".concat(t)})();
